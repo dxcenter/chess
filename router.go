c4ce63001e0bef34d338a11f7c30559d0a2fdc56
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-05-14 10:47:13 +0000

    Used nginx as a frontend

--- router.go
+++ router.go
@@ -6,8 +6,6 @@ import (
 )
 
 func setupRouter(r *gin.Engine) {
-	devMode := true
-
 	// My methods
 	r.GET("/ping.json",        m.Ping)
 	r.GET("/game_status.json", m.GameStatus)
@@ -15,31 +13,13 @@ func setupRouter(r *gin.Engine) {
 	r.POST("/move.json",       m.Move)
 
 	// Frontend
-	if !devMode {
-		r.Static("/frontend", "frontend/build")
-		r.Static("/static", "frontend/build/static")
-		r.Static("/css", "frontend/build/css")
-		r.StaticFile("/", "frontend/build/index.html")
-		r.StaticFile("/login", "frontend/build/index.html")
-		for _, file := range []string{"index.html", "service-worker.js"} {
-			r.StaticFile(file, "frontend/build/"+file)
-		}
-	}
-
-	// node.js
-	if devMode {
-		r.POST("/sockjs-node/*rest", m.ProxyToNodejs)
-		r.GET("/sockjs-node/*rest", m.ProxyToNodejs)
-		r.GET("/__webpack_dev_server__/*rest", m.ProxyToNodejs)
-
-		r.GET("/frontend/*rest", m.ProxyToNodejs)
-		r.GET("/static/*rest", m.ProxyToNodejs)
-		r.GET("/css/*rest", m.ProxyToNodejs)
-		r.GET("/", m.ProxyToNodejs)
-		r.GET("/login", m.ProxyToNodejs)
-		for _, file := range []string{"index.html", "service-worker.js"} {
-			r.GET("/"+file, m.ProxyToNodejs)
-		}
+	r.Static("/frontend", "frontend/build")
+	r.Static("/static", "frontend/build/static")
+	r.Static("/css", "frontend/build/css")
+	r.StaticFile("/", "frontend/build/index.html")
+	r.StaticFile("/login", "frontend/build/index.html")
+	for _, file := range []string{"index.html", "service-worker.js"} {
+		r.StaticFile(file, "frontend/build/"+file)
 	}
 
 	// JWT
