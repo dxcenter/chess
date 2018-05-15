af20e9bdd09bf26db37ca43d5b0172653cf13171
Dmitry Yu Okunev <dyokunev@ut.mephi.ru>
2018-05-15 15:21:27 +0000

    a bugfix of /whoami.json

--- serverMethods/middleware/authed.go
+++ serverMethods/middleware/authed.go
@@ -1,12 +1,25 @@
 package serverMethodsMiddleware
 
 import (
+	"database/sql"
 	m "github.com/dxcenter/chess/models"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func Authed(c *gin.Context) {
-	me, err := m.Player.First(c.GetInt("playerId"))
+	nickname := c.GetString("userID");
+	if nickname == "" {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": `nickname == ""`})
+		c.Abort()
+		return
+	}
+	me, err := m.Player.First(m.PlayerF{Nickname: &nickname})
+	if err == sql.ErrNoRows {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+		c.Abort()
+		return
+	}
 	if err != nil {
 		panic(err)
 	}
